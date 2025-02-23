45886b26fa04a16e6fca1d862917107e9d564216
Alexandre Dulaunoy <a@foo.be>
2025-02-23 10:39:41 +0100

    chg: [outils] première tentative pas trop mal pour un PDF du Sillon

--- outils/revue.sh
+++ outils/revue.sh
@@ -1,11 +1,5 @@
 export TEXINPUTS=.:../static/:
 python3 isbn.py --dump >full.md
-pandoc  -V footnotes-pretty=true -V table-use-row-color=true -V toc-own-page=true --template=book.tex -s --highlight-style=tango --highlight-style=monochrome --resource-path ../static/:. --toc --pdf-engine=xelatex -f gfm full.md -o revue.pdf
 #pandoc  --highlight-style=monochrome -V geometry:margin=1in --resource-path ../static/:. --toc --pdf-engine=xelatex -f gfm full.md -o revue.html
-#rm -rf tmp-revue
-#mkdir tmp-revue
-#cp -rf ../content/post/*.md ./tmp-revue
-#cp -rf ../static/images ./tmp-revue
-#cd tmp-revue
-#find . -type f -exec sed -i "/+++/,/+++/d" {} \;
-#pandoc --verbose -f markdown --metadata lang:fr *.md -o revue.pdf --pdf-engine=xelatex
+#pandoc  -V footnotes-pretty=true -V table-use-row-color=true -V toc-own-page=true --template=book.tex -s --highlight-style=tango --highlight-style=monochrome --resource-path ../static/:. --toc --pdf-engine=xelatex -f gfm full.md -o revue.pdf
+pandoc  --lua-filter=revue.lua -V book=true -V colorlinks=true -V linkcolor=blue  -V urlcolor=red -V toccolor=gray -V lang=fr -V footnotes-pretty=true -V table-use-row-color=true -V toc-own-page=true --template=eisvogel -s --highlight-style=tango --highlight-style=monochrome --resource-path ../static/:. --toc --pdf-engine=xelatex -f gfm full.md -o revue.pdf
